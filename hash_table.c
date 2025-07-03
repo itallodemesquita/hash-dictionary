ed209a39f7d8d04006990422ad2da4d1e5763abe
itallodemesquita <itallodemesquita@gmail.com>
2025-07-03 20:40:23 -0300

    feat: implementação de entradas na hash

--- hash_table.c
+++ hash_table.c
@@ -5,3 +5,10 @@ typedef struct Significado {
     char *definicao;
     struct Significado *proximo;
 } Significado;
+
+// Entrada na tabela hash
+typedef struct EntradaHash {
+    char *palavra;
+    Significado *significados; // Aponta pro início da lista
+    struct EntradaHash *próximo;
+} EntradaHash;
