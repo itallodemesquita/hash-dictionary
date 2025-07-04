013605c7ce0c876cb1ec1c0514d9c2cd90175344
itallodemesquita <itallodemesquita@gmail.com>
2025-07-04 09:58:53 -0300

    refactor: adição e inclusão de estruturas no header

--- hash_table.c
+++ hash_table.c
@@ -1,26 +1,9 @@
-#include <stdio.h>
-
-// Lista ligada de significados
-typedef struct Significado {
-    char *definicao;
-    struct Significado *proximo;
-} Significado;
-
-// Entrada na tabela hash
-typedef struct EntradaHash {
-    char *palavra;
-    Significado *significados; // Aponta pro início da lista
-    struct EntradaHash *próximo;
-} EntradaHash;
-
-// Estrutura para a hash
-typedef struct HashTable {
-    int tamanho;
-    EntradaHash **tabela; // Array de ponteiros para EntradaHash
-} TabelaHash;
+#include <stdlib.h>
+#include <string.h>
+#include "hash_table.h"
 
 // Função de Hash
-unsigned long funcaoHash(const char *str, int tam_tabela) {
+static unsigned long funcaoHash(const char *str, int tam_tabela) {
     
     unsigned long hash = 5381;
     int c;
---
+++ hash_table.h
@@ -0,0 +1,26 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stdio.h>
+
+// Lista ligada de significados
+typedef struct Significado {
+    char *definicao;
+    struct Significado *proximo;
+} Significado;
+
+// Entrada na tabela hash
+typedef struct EntradaHash {
+    char *palavra;
+    Significado *significados; // Aponta pro início da lista
+    struct EntradaHash *próximo;
+} EntradaHash;
+
+// Estrutura para a hash
+typedef struct HashTable {
+    int tamanho;
+    EntradaHash **tabela; // Array de ponteiros para EntradaHash
+} TabelaHash;
+
+
+#endif
